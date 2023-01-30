f8406a59af4f67286865a3704c7619af86ea5277
Caio Beneton <caio.beneton@gmail.com>
2023-01-30 09:10:09 -0300

    chore: app basic settings

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.use(json());
+
+app.get("/health", (req: Request, res: Response) => {
+    res.send("Ok")
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Running in port ${port}`))
